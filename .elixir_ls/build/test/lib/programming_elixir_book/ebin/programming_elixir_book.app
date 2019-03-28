27946b6e6e46ac1d9b7d3f2cbfb0888c9dbab49a
Dmitriy Ivliev <rootkit89@gmail.com>
2019-03-28 23:09:10 +0400

    try formatting and autocomplete

---
+++ .elixir_ls/build/test/lib/programming_elixir_book/ebin/programming_elixir_book.app
@@ -0,0 +1,6 @@
+{application,programming_elixir_book,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"programming_elixir_book"},
+              {modules,['Elixir.Parallel','Elixir.ProgrammingElixirBook']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- lib/exercise_one.exs
+++ lib/exercise_one.exs
@@ -1,7 +1,7 @@
-list_concat = fn (left, right) -> left ++ right end
-sum = fn (a, b, c) -> a + b + c end
-pair_tuple_to_list = fn ({a, b}) -> [a, b] end
+list_concat = fn left, right -> left ++ right end
+sum = fn a, b, c -> a + b + c end
+pair_tuple_to_list = fn {a, b} -> [a, b] end
 
-IO.puts(list_concat.([:a,:b], [:c, :d]) == [:a, :b, :c, :d])
+IO.puts(list_concat.([:a, :b], [:c, :d]) == [:a, :b, :c, :d])
 IO.puts(sum.(1, 2, 3) == 6)
 IO.puts(pair_tuple_to_list.({1234, 5678}) == [1234, 5678])
