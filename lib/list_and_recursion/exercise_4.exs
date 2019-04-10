8324324d2ee2da12559bc302c568c2da5a661ffe
Dmitriy Ivliev <rootkit89@gmail.com>
2019-04-10 17:35:10 +0400

    exercise 4; chapter list_and_recursion done

--- lib/list_and_recursion/exercise_4.exs
+++ lib/list_and_recursion/exercise_4.exs
@@ -1,16 +1,10 @@
 defmodule MyList do
+  def span(from, to) when from > to, do: []
+
   def span(from, to) do
-    span(from, to - 1, [to])
-  end
-
-  def span(from, to, []) when from == to do
-    [to | list]
-  end
-
-  def span(from, to, list) do
-    span(from, to - 1, [to | list])
+    [from | span(from + 1, to)]
   end
 end
 
 IO.puts(MyList.span(2, 5) == [2, 3, 4, 5])
-# IO.puts(MyList.span(5, 5) == [2, 3, 4, 5]) # TODO: fix infity loop
+IO.puts(MyList.span(5, 5) == [5])
