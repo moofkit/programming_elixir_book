8d250a7b590a122366323d124c2bd22a5956fa43
Dmitriy Ivliev <rootkit89@gmail.com>
2019-04-17 20:17:31 +0400

    exercise 5; chapter list_and_recursion finish

--- lib/list_and_recursion/exercise_5.exs
+++ lib/list_and_recursion/exercise_5.exs
@@ -24,8 +24,30 @@ defmodule MyEnum do
       filter(tail, fun, acc)
     end
   end
+
+  def split(list, _count, acc \\ [])
+
+  def split(list, count, acc) when count == 0 do
+    [Enum.reverse(acc), list]
+  end
+
+  def split([head | tail], count, acc) do
+    split(tail, count - 1, [head | acc])
+  end
+
+  def take(list, _count, acc \\ [])
+
+  def take(_list, count, acc) when count == 0 do
+    Enum.reverse(acc)
+  end
+
+  def take([head | tail], count, acc) do
+    take(tail, count - 1, [head | acc])
+  end
 end
 
 IO.puts(MyEnum.all?([2, 4, 6], &Integer.is_even/1) == true)
 MyEnum.each(["hello", "world"], fn word -> IO.puts(word) end)
 IO.puts(MyEnum.filter([1, 2, 3, 4, 5], &Integer.is_even/1) == [2, 4])
+IO.puts(MyEnum.split([1, 2, 3, 4, 5], 3) == [[1, 2, 3], [4, 5]])
+IO.puts(MyEnum.take([1, 2, 3, 4, 5], 2) == [1, 2])
